62364aef7336253ba9d344b79bb51e314b5a77c2
sakiiic <44774179+sakiiic@users.noreply.github.com>
2020-03-30 23:35:22 +0200

    routes

--- PUSGSWeb/src/app/app-routing.module.ts
+++ PUSGSWeb/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { AviokompanijaComponent } from './components/aviokompanija/aviokompanija.component';
+import { RentACarComponent } from './components/rent-acar/rent-acar.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: 'aviokompanije', component: AviokompanijaComponent },
+  { path: 'rentacar', component: RentACarComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
