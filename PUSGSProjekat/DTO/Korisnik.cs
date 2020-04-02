8032df2e703ea35d29d2f37a44c5881688e69535
SlobodankaJovic <43542423+SlobodankaJovic@users.noreply.github.com>
2020-04-02 17:44:52 +0200

    Korisnik & login

---
+++ PUSGSProjekat/DTO/Korisnik.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PUSGSProjekat.DTO
+{
+    public class Korisnik
+    {
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+        public string KorisnickoIme { get; set; }
+        public string Lozinka { get; set; }
+    }
+}
---
+++ PUSGSWeb/src/app/models/korisnik.model.ts
@@ -0,0 +1,6 @@
+export class KorisnikModel{
+    ime: string;
+    prezime: string;
+    korisnickoIme: string;
+    lozinka: string;
+}
