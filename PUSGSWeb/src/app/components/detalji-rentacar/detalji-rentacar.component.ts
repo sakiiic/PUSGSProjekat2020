dfdc54ad4f09a1a5c90f8c74c84b3c070faa995f
sakiiic <44774179+sakiiic@users.noreply.github.com>
2020-04-14 22:00:44 +0200

    commit

--- PUSGSWeb/src/app/components/detalji-rentacar/detalji-rentacar.component.ts
+++ PUSGSWeb/src/app/components/detalji-rentacar/detalji-rentacar.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { RentACarService } from 'src/app/services/rent-a-car/rent-a-car.service';
 import { MatTableDataSource } from '@angular/material/table';
 import { RentACarModel } from 'src/app/models/rentACar.model';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-detalji-rentacar',
@@ -12,13 +13,17 @@ export class DetaljiRentacarComponent implements OnInit {
 
   displayedColumns = ['naziv', 'marka', 'model', 'godinaProizvodnje', 'brojSjedista', 'tipVozila', 'detalji'];
   dataSource: any;
+  id: any;
 
-  constructor(public service: RentACarService) { 
+  constructor(public service: RentACarService, 
+    private activatedRoute: ActivatedRoute) { 
     this.dataSource = new MatTableDataSource<RentACarModel>();
+    if (this.activatedRoute.snapshot.params["id"])
+    this.id = this.activatedRoute.snapshot.params["id"];
   }
 
   ngOnInit(): void {
-    this.service.getRentACarInfo(1)
+    this.service.getRentACarInfo(this.id)
          .subscribe((res: RentACarModel[]) => {
             console.log(res)
             this.dataSource = res;
