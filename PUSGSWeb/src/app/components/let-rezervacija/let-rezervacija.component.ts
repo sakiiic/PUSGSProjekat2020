edf0c3b88911b137adc4685afeb1e08104ab1da2
SlobodankaJovic <43542423+SlobodankaJovic@users.noreply.github.com>
2020-07-28 11:30:48 +0200

    Flight - filter

--- PUSGSWeb/src/app/components/let-rezervacija/let-rezervacija.component.ts
+++ PUSGSWeb/src/app/components/let-rezervacija/let-rezervacija.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { LetService } from 'src/app/services/let/let.service';
+import { LetModel } from 'src/app/models/let.model';
+import { Router } from '@angular/router';
+import { msToTimeString } from 'src/app/util/time';
+
 
 @Component({
   selector: 'app-let-rezervacija',
@@ -8,9 +13,81 @@ import { Component, OnInit } from '@angular/core';
 
 export class LetRezervacijaComponent implements OnInit {
 
-  constructor() { }
+  allFlights: any;
+  displayedFlights: LetModel[];
+  search = "";
+  from = "";
+  to = "";
+  price: number;
+
+  constructor(public letService: LetService, private router: Router) { }
+
+  //ngOnInit(): void {}
 
   ngOnInit(): void {
+    this.letService.getLetovi().subscribe(items => {
+      this.allFlights = items;
+      this.displayedFlights = this.allFlights;
+      console.log("Loaded flights", this.allFlights);
+    });
+  }
+
+  onApplyFilter() {
+    let query = this.allFlights;
+
+    this.search.split(',').forEach(str => {
+      const search = str.toLowerCase().trim();
+      if (search) {
+        query = this.filterBySearchText(query, search);
+      }
+    })
+
+    if (this.price) {
+      query = this.filterByPrice(query, this.price);
+    }
+
+    if (this.from) {
+      query = this.filterByFromDate(query, this.from);
+    }
+    if (this.to) {
+      query = this.filterByToDate(query, this.to);
+    }
+
+    this.displayedFlights = query;
+  }
+
+  private filterBySearchText(query: LetModel[], search = ""): LetModel[] {
+    return query.filter(flight => {
+      return flight.destinacija.toLowerCase().includes(search);
+    });
+  }
+
+  private filterByPrice(query: LetModel[], price: number): LetModel[] {
+    return query.filter(flight => {
+      return flight.cijenaKarte <= price;       
+    });
+  }
+
+  private filterByFromDate(query: LetModel[], dateStr = ""): LetModel[] {
+    try {
+      const date = new Date(dateStr);
+      console.log("from", date);
+      return query.filter(flight => new Date(flight.datumVrijemePolaska).getTime() >= date.getTime());
+    } catch (ex) {
+      console.warn(ex);
+    }
+    return query;
+  }
+
+  private filterByToDate(query: LetModel[], dateStr = ""): LetModel[] {
+    try {
+      const date = new Date(dateStr);
+      console.log("to", date);
+      return query.filter(flight => new Date(flight.datumVrijemePolaska).getTime() <= date.getTime());
+    } catch (ex) {
+      console.warn(ex);
+    }
+    return query;
   }
 
 }
---
+++ PUSGSWeb/src/app/models/flight.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Flight = /** @class */ (function () {
+    function Flight() {
+        this.departureTime = new Date();
+        this.arrivalTime = new Date();
+        this.averageRating = 0;
+    }
+    return Flight;
+}());
+exports.Flight = Flight;
+//# sourceMappingURL=flight.js.map
